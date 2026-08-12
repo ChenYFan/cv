a914aa450f51b4b0c4ba6a6849c32aab31355ccf
CyanFalse <chen2778754364@foxmail.com>
2026-08-13 01:53:02 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 // import CYAN_VAR from './var.json'
 let CYAN_VAR;
 
-if (process.env.RESUME_VAR_JSON) CYAN_VAR = JSON.parse(process.env.RESUME_VAR_JSON).replace(/\<\|\|\>/g, " ");
+if (process.env.RESUME_VAR_JSON) CYAN_VAR = JSON.parse(process.env.RESUME_VAR_JSON.replace(/\<\|\|\>/g, " "));
 else if (fs.existsSync('./var.json')) CYAN_VAR = JSON.parse(fs.readFileSync('./var.json', 'utf-8'));
 else throw new Error('Missing data source: RESUME_VAR_JSON or var.json');
 
